557420e9c82ff21756813430f8a5f4ca3aac247c
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2026-07-21 23:10:33 +0530

    JavaFX 26.0.2 release (#283)

--- js/gluon.js
+++ js/gluon.js
@@ -5,7 +5,7 @@ $(function() {
     var JDK_MAJOR = "24";
     var JFX_MAJOR = "26";
     
-    var JFX_VERSION = "26.0.1";
+    var JFX_VERSION = "26.0.2";
     var JFX_LTS_VERSION = "17 or 21";
     var JFX_PLUGIN_VERSION = "0.1.0";
     var JFX_MVN_PLUGIN_VERSION = "0.0.8";
